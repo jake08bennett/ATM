f6fb49e4465554870370c29a9677fde875e51dbd
jake08bennett <jake08bennett@hotmail.com>
2015-11-13 10:13:29 -0700

    bants

--- account.rb
+++ account.rb
@@ -8,23 +8,11 @@ class Account
 		@pin = pin
 	end
 
-	def withdraw 
-		if @pin == pin
-			puts "How much would you like to withdraw?"
-			amountW = gets
-			puts "withdraw $" + amountW.to_s
-			@money -= amountW.to_i
-			puts @money
-		end
+	def withdraw_value(amountW)
+		@money -= amountW.to_i
 	end
 
-	def deposit
-		if @pin == pin
-			puts "How much would you like to deposit?"
-			amountD = gets
-			puts "Deposit $" + amountD.to_s
-			@money += amountD.to_i
-			puts @money
-		end
+	def deposit_value(amountD)
+		@money += amountD.to_i
 	end
 end
